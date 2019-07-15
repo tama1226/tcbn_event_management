6fb563f93f68b1ab9992cbb4bb7f9c0bce045407
hiroaki tamaki <hiro6221uzuz@gmail.com>
2019-07-16 03:20:31 +0900

    add : 簡易的なトップページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
+  root  'events#toppage.html'
+
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
 
   resource :contacts, only: [:new, :create]
   
-  root 'events#index'
   resources :events
 
   if Rails.env.development?
