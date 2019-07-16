23603e7df933b8ffd4ce802908cf05cc2733d3af
hiroaki tamaki <hiro6221uzuz@gmail.com>
2019-07-16 16:03:06 +0900

    add : commentsコントローラーの機能追加

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,20 @@
+class CommentsController < ApplicationController
+  def create
+    @event = Event.find(pramas[:evnet_id])
+    @comment = @event.comments.build(comment_params)
+
+    respond_to do |format|
+      if @comment.save
+        format.js{render :index}
+      else
+        format.html {redirect_to event_path(@event), notice:'投稿不可！'}
+      end  
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:event_id,:content)
+  end
+end
