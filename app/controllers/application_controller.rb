8d41c1396c3683b4005b9022e895378aad8b2b23
hiroaki tamaki <hiro6221uzuz@gmail.com>
2019-07-20 22:09:07 +0900

    add : ログインユーザーのプロフィール画面

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,13 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
   
-  #新規登録時にnameカラムのパラメーターを許可する
+  #新規登録時にパラメーターを許可する
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys:[:name, :phone_number])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :phone_number])
+  end
+  #ログイン後に遷移するページ指定
+  def after_sign_in_path_for(resource)
+    events_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
+
   root  'events#toppage.html'
 
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  devise_for :users
+
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
+  resources :users, only: [:show]
 
   resource :contacts, only: [:new, :create]
   
