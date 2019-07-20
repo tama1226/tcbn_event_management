c319fda8f342d1f5788aa55ae4d47192f42de526
hiroaki tamaki <hiro6221uzuz@gmail.com>
2019-07-20 16:23:25 +0900

    modify : Userモデルのバリデーションを修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  validates :name, presence: true, length: { in: 1..30 }
+  validates :phone_number,
+             format: { with: /\A\d{10,11}\z/ }
+  validates :email, presence: true, length: { minimum: 6 },
+             uniqueness: true,
+             format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
 
   has_many :events, dependent: :destroy
   has_many :comments, dependent: :destroy
