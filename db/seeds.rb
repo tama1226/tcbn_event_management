950afede5064eae20ff94ecba3868e85bd2b492f
hiroaki tamaki <hiro6221uzuz@gmail.com>
2019-08-07 00:49:04 +0900

    add : seedデータ作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+20.times do |n|
+  Event.create(name: "event_name",
+              kind: "イベント",
+              price: "2000",
+              game: "3",
+              lane: "18")
+end
+
+User.create(name: "admin",
+            admin: "true",
+            email: "admin@example.com",
+            password: "password",
+            password_confirmation: "password")
+
+User.create(name: "sample",
+            admin: "false",
+            email: "sample@example.com",
+            password: "sample",
+            password_confirmation: "sample")
