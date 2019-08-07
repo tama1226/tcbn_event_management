d6df5e2e5b4a165f4c0cfd696764656f45337139
hiroaki tamaki <hiro6221uzuz@gmail.com>
2019-08-07 19:03:47 +0900

    modify : プロフィールは本人のみ閲覧可能

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    #プロフィールはログインユーザーしか見れない
+    unless @user.id == current_user.id
+      redirect_to events_path
+      flash[:notice] = '本人しか閲覧できません'
+    end
   end
 end
