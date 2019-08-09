5e138de8b95acf7d8c8ae8ac8172c51fd2b8358e
hiroaki tamaki <hiro6221uzuz@gmail.com>
2019-08-09 20:12:13 +0900

    modify : プロフィール閲覧の挙動をprivate下に記載

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,12 @@
 class UsersController < ApplicationController
+  before_action :login_prof
+  
   def show
+  end
+
+  private
+
+  def login_prof
     @user = User.find(params[:id])
     #プロフィールはログインユーザーしか見れない
     unless @user.id == current_user.id
